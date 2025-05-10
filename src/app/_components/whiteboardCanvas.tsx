3fc61d1f9cc693350ae67fbe861dbb318f03f337
Mieko Hikari <77004524+MiekoHikari@users.noreply.github.com>
2025-05-10 09:56:43 +0000

    Implement controls (very buggy)

--- src/app/_components/whiteboardCanvas.tsx
+++ src/app/_components/whiteboardCanvas.tsx
@@ -1,5 +1,6 @@
 "use client";
-import React, { useEffect, useRef, useState } from "react";
+import type React from "react";
+import { useEffect, useRef, useState } from "react";
 
 function WhiteboardCanvas() {
 	const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -7,8 +8,8 @@ function WhiteboardCanvas() {
 	const [drawing, setDrawing] = useState(false);
 	const [currentColor, setCurrentColor] = useState("#000000");
 	const [lineWidth, setLineWidth] = useState(3);
-	const [drawingActions, setDrawingActions] = useState([]);
-	const [currentPath, setCurrentPath] = useState([]);
+	const [drawingActions, setDrawingActions] = useState<{ path: { x: number; y: number }[]; style: { color: string; lineWidth: number } }[]>([]);
+	const [currentPath, setCurrentPath] = useState<{ x: number; y: number }[]>([]);
 	const [currentStyle, setCurrentStyle] = useState({
 		color: "#000000",
 		lineWidth: 3,
@@ -26,11 +27,108 @@ function WhiteboardCanvas() {
 		}
 	});
 
-	return <div>WhiteboardCanvas</div>;
+	const startDrawing = (e: React.MouseEvent) => {
+		if (context) {
+			context.beginPath();
+			context.moveTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+			setDrawing(true);
+		}
+	};
+
+	const draw = (e: React.MouseEvent) => {
+		if (!drawing) return;
+		if (context) {
+			context.strokeStyle = currentStyle.color;
+			context.lineWidth = currentStyle.lineWidth;
+			context.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+			context.stroke();
+			setCurrentPath([...currentPath, { x: e.nativeEvent.offsetX, y: e.nativeEvent.offsetY }]);
+		}
+	};
+
+	const endDrawing = () => {
+		setDrawing(false);
+		if (context) context.closePath();
+
+		if (currentPath.length > 0) {
+			setDrawingActions([...drawingActions, { path: currentPath, style: currentStyle }]);
+		}
+	}
+
+	const changeColor = (color: string) => {
+		setCurrentColor(color);
+		setCurrentStyle({ ...currentStyle, color });
+	};
+
+	const changeWidth = (width: number) => {
+		setLineWidth(width);
+		setCurrentStyle({ ...currentStyle, lineWidth: width });
+	}
+
+	const undoDrawing = () => {
+		if (drawingActions.length > 0) {
+			drawingActions.pop();
+
+			if (canvasRef.current) {
+				const newContext = canvasRef.current?.getContext("2d");
+				
+				if (newContext) {
+					newContext?.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+
+					for (const { path, style } of drawingActions) {
+						newContext.beginPath();
+						newContext.strokeStyle = style.color;
+						newContext.lineWidth = style.lineWidth;
+						path[0] && newContext.moveTo(path[0].x, path[0].y);
+
+						for (const point of path) {
+							newContext.lineTo(point.x, point.y);
+						}
+
+						newContext.stroke();
+					}
+				}
+			}
+		}
+	}
+
+	const clearDrawing = () => {
+		setDrawingActions([]);
+		setCurrentPath([]);
+
+		const newContext = canvasRef.current?.getContext("2d");
+		if (newContext) {
+			canvasRef.current && newContext.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+		}
+	}
+
+	const reDrawPreviousData = (ctx: CanvasRenderingContext2D | null) => {
+		for (const {path, style} of drawingActions) {
+			if (ctx) {
+				ctx.beginPath();
+				ctx.strokeStyle = style.color;
+				ctx.lineWidth = style.lineWidth;
+				path[0] && ctx.moveTo(path[0].x, path[0].y);
+
+				for (const point of path) {
+					ctx.lineTo(point.x, point.y);
+				}
+
+				ctx.stroke();
+			}
+		}
+	}
+
+	return <div>
+		<canvas
+			ref={canvasRef}
+			onMouseDown={startDrawing}
+			onMouseMove={draw}
+			onMouseUp={endDrawing}
+			onMouseOut={endDrawing}
+			onBlur={endDrawing}
+			className="border border-gray-400" />
+	</div>;
 }
 
 export default WhiteboardCanvas;
-
-function reDrawPreviousData(ctx: CanvasRenderingContext2D | null) {
-	throw new Error("Function not implemented.");
-}
