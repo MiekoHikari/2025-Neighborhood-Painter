3c9caa3e6a7ea929744c998ddfcd4f88cc31b8b3
Mieko Hikari <77004524+MiekoHikari@users.noreply.github.com>
2025-05-11 12:39:43 +0000

    Allocated space for search in the dashboard

--- src/app/(dashboard)/_components/navbar.tsx
+++ src/app/(dashboard)/_components/navbar.tsx
@@ -7,10 +7,8 @@ async function Navbar() {
 
 	return (
 		<div className="flex items-center gap-x-4 bg-green-500 p-5">
-			<div>Navbar</div>
-			<div>
-				<UserButton session={session} />
-			</div>
+			<div className="hidden bg-yellow-500 lg:flex lg:flex-1">Navbar</div>
+			<UserButton session={session} />
 		</div>
 	);
 }
