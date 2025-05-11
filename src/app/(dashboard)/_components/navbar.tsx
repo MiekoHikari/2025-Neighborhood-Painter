236cfb8ed3b2e7862fa3819cc37c2ceddd4bdedb
Mieko Hikari <77004524+MiekoHikari@users.noreply.github.com>
2025-05-11 04:21:53 +0000

    Create a basic dashboard layout

--- src/app/(dashboard)/_components/navbar.tsx
+++ src/app/(dashboard)/_components/navbar.tsx
@@ -2,7 +2,9 @@
 import React from "react";
 
 function Navbar() {
-	return <div className="flex items-center gap-x-4 p-5 bg-green-500">Navbar</div>;
+	return (
+		<div className="flex items-center gap-x-4 bg-green-500 p-5">Navbar</div>
+	);
 }
 
 export default Navbar;
